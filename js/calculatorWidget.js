49e117ca4e6a72540d66837e1d94e37926b2d828
Zevest <38529923+Zevest@users.noreply.github.com>
2019-04-10 14:57:51 +0200

    Fixed 1.01+ 2.02 bug

--- js/calculatorWidget.js
+++ js/calculatorWidget.js
@@ -126,14 +126,14 @@ class CalculatorModel extends WidgetModel {
       }
       if (a >= 1 && b >= 1) {
         if (a > b) {
-          copy.splice(b - 1, 3, lst[b - 1] / lst[b + 1]);
+          copy.splice(b - 1, 3, this.fix(10, lst[b - 1] / lst[b + 1]));
         } else {
-          copy.splice(a - 1, 3, lst[a - 1] * lst[a + 1]);
+          copy.splice(a - 1, 3, this.fix(10, lst[a - 1] * lst[a + 1]));
         }
       } else if (a >= 1) {
-        copy.splice(a - 1, 3, lst[a - 1] * lst[a + 1]);
+        copy.splice(a - 1, 3, this.fix(10, lst[a - 1] * lst[a + 1]));
       } else if (b >= 1) {
-        copy.splice(b - 1, 3, lst[b - 1] / lst[b + 1]);
+        copy.splice(b - 1, 3, this.fix(10, lst[b - 1] / lst[b + 1]));
       }
       i++;
       if (i > this.numberMax) {
@@ -157,14 +157,14 @@ class CalculatorModel extends WidgetModel {
       }
       if (a >= 1 && b >= 1) {
         if (a > b) {
-          copy.splice(b - 1, 3, lst[b - 1] - lst[b + 1]);
+          copy.splice(b - 1, 3, this.fix(10, lst[b - 1] - lst[b + 1]));
         } else {
-          copy.splice(a - 1, 3, lst[a - 1] + lst[a + 1]);
+          copy.splice(a - 1, 3, this.fix(10, lst[a - 1] + lst[a + 1]));
         }
       } else if (a >= 1) {
-        copy.splice(a - 1, 3, lst[a - 1] + lst[a + 1]);
+        copy.splice(a - 1, 3, this.fix(10, lst[a - 1] + lst[a + 1]));
       } else if (b >= 1) {
-        copy.splice(b - 1, 3, lst[b - 1] - lst[b + 1]);
+        copy.splice(b - 1, 3, this.fix(10, lst[b - 1] - lst[b + 1]));
       }
       i++;
       if (i > this.numberMax) {
@@ -214,7 +214,7 @@ class CalculatorModel extends WidgetModel {
       if (typeof par == "string") return par;
       let a = par[0][0],
         b = par[0][1];
-      copy.splice(a, b - a + 1, this.fix(10,this.calculate(copy.slice(a, b + 1))));
+      copy.splice(a, b - a + 1, this.calculate(copy.slice(a, b + 1)));
     }
     return copy[0];
   }
